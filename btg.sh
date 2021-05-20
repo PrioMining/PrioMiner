9d2b6f0ce7bd3fd361a5d9a8dbd3e801d0f2973d
mabay27 <50239988+mabay27@users.noreply.github.com>
2021-05-20 19:43:13 +0700

    Update btg.sh

--- btg.sh
+++ btg.sh
@@ -1,5 +1,5 @@
 #btg.sh
-sudo apt update 
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+apt update 
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
 tar -xzvf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a yescrypt -o stratum+tcp://yespower.na.mine.zpool.ca:6234  -u AdLME4saaPgwDJgZaBxcfVvcEvtHF3kY9G -p c=BTG,zap=BELL/CRP/PYRK-yespower/VECO
+./cpuminer-sse2 -a yescrypt -o stratum+tcp://yespower.na.mine.zpool.ca:6234 -u AdLME4saaPgwDJgZaBxcfVvcEvtHF3kY9G -p c=BTG,zap=BELL/CRP/PYRK-yespower/VECO
