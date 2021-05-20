8d140412acfac0abf43b53c5f70b362cfaabafdd
mabay27 <50239988+mabay27@users.noreply.github.com>
2021-05-20 21:16:59 +0700

    Update btg.sh

--- btg.sh
+++ btg.sh
@@ -1,5 +1,5 @@
 #btg.sh
 apt update 
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.21/cpuminer-opt-linux.tar.gz
-tar -xzvf cpuminer-opt-linux.tar.gz && 
+tar -xzvf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.na.mine.zpool.ca:6240 -u AdLME4saaPgwDJgZaBxcfVvcEvtHF3kY9G -p c=BTG,zap=CPU
